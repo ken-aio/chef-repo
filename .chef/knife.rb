bf9cb542d6fe2ff085140455494f9ac3fda86cc8
Suguru Akiho <suguru.akiho@gmail.com>
2014-03-25 04:51:56 -0400

    first commit

---
+++ .chef/knife.rb
@@ -0,0 +1,13 @@
+# See http://docs.opscode.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "ken_aio"
+client_key               "#{current_dir}/ken_aio.pem"
+validation_client_name   "circleboard-validator"
+validation_key           "#{current_dir}/circleboard-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/circleboard"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
